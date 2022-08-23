ac249a8f9fae433d62ba2b96dd37e6c9665001d9
MANOJ GOWDA VENKATACHALA MURTHY <manojgowdavenkatachalamurthy@MANOJs-MacBook-Air.local>
2022-08-22 23:15:57 -0400

    first commit

---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Announcements from '../components/Announcements'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
+const Landingpage = () => {
+  return (
+    <div>
+    <Announcements />
+    <Navbar />
+    <Slider/>
+    </div>
+  )
+}
+
+export default Landingpage
